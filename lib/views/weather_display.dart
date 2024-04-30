700e6bdd77e08a3d069f6be7b7b43f0e1f4b793a
Taaha Rauf <taaha161@gmail.com>
2024-04-30 15:14:39 +0500

    Weather with Future Builder

--- lib/views/weather_display.dart
+++ lib/views/weather_display.dart
@@ -1,3 +1,4 @@
+import 'package:diraction/controller/weather_controller.dart';
 import 'package:diraction/models/weather_model.dart';
 import 'package:flutter/material.dart';
 
@@ -8,29 +9,43 @@ class WeatherDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(weatherData.cityName ?? "City Not Found"),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Center(
-            child:
-                Text(weatherData.temperatureC.toString() ?? "City Not Found"),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Center(
-            child:
-                Text(weatherData.temperatureF.toString() ?? "City Not Found"),
-          ),
-        ],
-      ),
-    );
+        backgroundColor: Colors.white,
+        body: FutureBuilder(
+          future: WeatherController.fetchData("lAHORE"),
+          builder: ((context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text("An error has occurred while fetching data"),
+              );
+            } else {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(snapshot.data!.cityName ?? "City Not Found"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(snapshot.data!.temperatureC.toString() ??
+                        "City Not Found"),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Center(
+                    child: Text(snapshot.data!.temperatureC.toString() ??
+                        "City Not Found"),
+                  ),
+                ],
+              );
+            }
+          }),
+        ));
   }
 }
